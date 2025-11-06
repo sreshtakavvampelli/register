9caec8128b815c976f2fb239f17d4c6e9a9333e0
sreshtakavvampelli <kavvampellisreshta11@gmail.com>
2025-11-06 16:12:19 +0530

    break command is added

---
+++ swap.java
@@ -0,0 +1,10 @@
+public class swap{
+public static void main(String args[]){
+int a=8,b=14,temp;
+System.out.println("before swapping:"a="+a+",b="+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("after swapping:",a=+"a+",b="+b);
+}
+}
